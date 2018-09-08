e3ad42e138f21e4a3e298d37a9e4f60a67cf022f
ANTONIO FRANCISCO DA ROSA ALVES <afralves@MacBook-Air-de-ANTONIO.local>
2018-09-07 21:20:28 -0300

    my_money projeto

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default props => (
+
+    <footer className='main-footer'>
+        <strong>Copyright &copy; 2018
+        <a href='http://localhost:8080/#' target='_blank'> Afralves</a>.</strong>
+    </footer>
+)
